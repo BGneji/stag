0a88e70bda3777a7caedfa9580268d3c30df78ed
NEJI <mrkochetov@yandex.ru>
2024-12-05 15:47:26 +0300

    Уведомление о сильных колебаниях check_price_difference

--- project_1/data_download.py
+++ project_1/data_download.py
@@ -4,6 +4,8 @@ import yfinance as yf
 def fetch_stock_data(ticker, period='1mo'):
     stock = yf.Ticker(ticker)
     data = stock.history(period=period)
+    print(data)
+    data.to_excel('max_dates.xlsx', index=False)
     return data
 
 
@@ -16,8 +18,18 @@ def calculate_and_display_average_price(data, ticker):
     """Вывод средней цены за период"""
     avg_close = round(data['Close'].mean(), 4)
     print(f'Средняя цена закрытия акций {ticker}: {avg_close}')
+    print()
 
+def check_price_difference(data, threshold):
+    """Уведомление о сильных колебаниях, указать порог"""
+    max_close = data['Close'].max()
+    min_close = data['Close'].min()
+    difference = round((max_close - min_close), 4)
+    print(f"Максимальная цена закрытия: {max_close}")
+    print(f"Минимальная цена закрытия: {min_close}")
+    print(f"Разница: {difference}")
 
-
-
-
+    if difference > threshold:
+        print(f"Уведомление: Разница {difference} превышает порог {threshold}.")
+    else:
+        print(f"Разница {difference} не превышает порог {threshold}.")
--- project_1/project_1_main.py
+++ project_1/project_1_main.py
@@ -7,21 +7,24 @@ def main():
     print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
     print("Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
 
-    ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
+    ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc): ")
     period = input("Введите период для данных (например, '1mo' для одного месяца): ")
+    threshold = int(input("Введите значение порога колебания: "))
 
     # Fetch stock data
     """Получить данные"""
     stock_data = dd.fetch_stock_data(ticker, period)
+    """Вывод средней цены за период"""
     stock_data_avg = dd.calculate_and_display_average_price(stock_data, ticker)
-
+    """Уведомление о сильных колебаниях, указать порог"""
+    dd.check_price_difference(stock_data, threshold)
     # Add moving average to the data
     """Добавьте скользящее среднее к данным"""
     stock_data = dd.add_moving_average(stock_data)
 
     # Plot the data
     """Создание графика"""
-    dplt.create_and_save_plot(stock_data, ticker, period, stock_data_avg)
+    dplt.create_and_save_plot(stock_data, ticker, period)
 
 
 
